3f593f0b9b62c02cf10222e491bb3049bf8bbc0e
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-11-25 22:45:56 -0500

    refactor routing.py

---
+++ view/app/routing.py
@@ -0,0 +1,33 @@
+from dash import dcc
+from dash.dependencies import Input, Output
+from ..home.layout import create_home_layout
+from ..intervenciones_simultaneas.layout import create_intervenciones_simultaneas_layout
+from ..puentes_criticos.layout import create_puentes_criticos_layout
+
+def create_app_routing(app):
+    """
+    Create location component 
+    Add routing callbacks for the app
+    """
+
+    # Create location component
+    location = dcc.Location(id='url', refresh=False),
+    
+    # Create routing layouts 
+    home_layout = create_home_layout(app)
+    puentes_criticos_layout = create_puentes_criticos_layout(app)
+    intervenciones_simultaneas_layout = create_intervenciones_simultaneas_layout(app)
+
+    @app.callback(
+        Output('page-content', 'children'), 
+        Input('url', 'pathname'),
+    )
+    def display_page(pathname):
+        if pathname == '/puentes_criticos':
+            return puentes_criticos_layout
+        elif pathname == '/intervenciones-simultaneas':
+            return intervenciones_simultaneas_layout
+        else:
+            return home_layout
+
+    return location
