d9ce3c2766c17190274d83742e75d0584ea4167e
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-11-25 22:52:32 -0500

    App layout refactor

---
+++ view/app/layout.py
@@ -0,0 +1,23 @@
+from dash import html
+from .routing import create_app_routing
+from ..navbar.layout import create_navbar_layout
+from ..content.layout import create_content_layout
+
+def create_app_layout(app):
+    """
+    Create layout component of the app
+    """
+    
+    # Retrieve components
+    location = create_app_routing(app)
+    navbar = create_navbar_layout(app)
+    content = create_content_layout(app)
+
+    # Create layout
+    layout = html.Div([
+        location,
+        navbar,
+        content,
+    ])
+    
+    return layout
