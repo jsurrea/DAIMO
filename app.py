5b6b42d37fc81e4cf8578d40a88f50c7c890ef12
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-11-25 22:07:02 -0500

    refactor app.py

--- app.py
+++ app.py
@@ -1,52 +1,20 @@
 import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-import plotly.express as px
+from view.base.layout import create_layout
 
-from view.navbar_view import create_navbar
-from view.sidebar_view import create_sidebar
-from view.content_view import create_content
-from view.modal_view import create_modal
-
-
-def run_app(debug = False):    
+def run_app(debug = False):
+    """
+    Create and run the app on port 8086
+    debug: True if you want to run the app in debug mode
+    """
     
-    app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+    # Create app
+    app = dash.Dash(external_stylesheets = [dbc.themes.BOOTSTRAP])
     app.title = "DAIMO"
-    
-    # Retrieve components
-    
-    content = create_content(app)
-    sidebar = create_sidebar(app)
-    navbar = create_navbar(app)
-    modal = create_modal(app)
-    #home = create_home(app)
-    
-    app.layout = html.Div(
-        [
-            dcc.Location(id='url', refresh=False),
-            navbar,
-            html.Div(id='page-content'),
-            #sidebar,
-            #modal,
-            #content,
-        ],
-    )
-
-    # Routing
-    @app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
-    def display_page(pathname):
-        if pathname == '/page-1':
-            return html.P("This is Page 1")
-        elif pathname == '/page-2':
-            return html.P("This is Page 2")
-        else:
-            return html.P("This is the default page")
+    app.layout = create_layout(app)
         
+    # Run app
     app.run_server(debug=debug, port=8086)
-
 
 
 if __name__ == "__main__":
