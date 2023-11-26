b3cca4bfc33b89c3c1b537213e557c508e558b7a
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-11-25 23:59:12 -0500

    puentes_criticos component v1

---
+++ view/puentes_criticos/layout.py
@@ -0,0 +1,10 @@
+from dash import html
+
+def create_puentes_criticos_layout(app):
+    """
+    Create layout of the puentes_criticos component
+    """
+
+    puentes_criticos = html.P("Puentes Críticos")
+
+    return puentes_criticos
